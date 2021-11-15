6381da182fc020db4d0f56af1d0672d66389c9e6
Math23k5 <93868847+Math23k5@users.noreply.github.com>
2021-11-15 13:08:48 +0100

    Undid recent change

--- TowerDefenseV2/TowerDefenseV2/Enemy.cs
+++ TowerDefenseV2/TowerDefenseV2/Enemy.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Content;
 
 namespace Tower_Defense
 {
-    public abstract class Enemy : GameWorld
+    public abstract class Enemy
     {
         // Variabler/fields
         private Vector2 position;
